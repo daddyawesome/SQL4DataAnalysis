ccfd73310dc234a422644b6dc54784a6b28c85a1
DaddyAwesome <sablay296@gmail.com>
2020-02-20 18:39:58 +0000

    lesson 1 - udacity sql

--- lesson1.sql
+++ lesson1.sql
@@ -35,8 +35,16 @@ FROM orders
 ORDER BY total
 LIMIT 20;
 
+-- QUIZ ORDER BY 
+-- 1. Write a query that displays the order ID, account ID, and total dollar amount for all the orders, sorted first by the account ID (in ascending order), and then by the total dollar amount (in descending order).
 
+-- 2. Now write a query that again displays order ID, account ID, and total dollar amount for each order, but this time sorted first by total dollar amount (in descending order), and then by account ID (in ascending order).
 
+-- 3. Compare the results of these two queries above. How are the results different when you switch the column you sort on first?
+
+SELECT id, account_id, total_amt_usd
+FROM orders
+ORDER BY account_id, total_amt_usd DESC;
 
 
 /*
